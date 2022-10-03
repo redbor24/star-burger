ccf5172d9a32b462ff1f69839f16240d025aad98
redbor <redbor24@gmail.com>
2022-10-03 11:49:54 +0300

    Разрешены null в полях широты и долготы

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -229,8 +229,8 @@ class OrderLines(models.Model):
 
 class Location(models.Model):
     address = models.CharField(max_length=100, verbose_name='Адрес', db_index=True, unique=True)
-    lat = models.FloatField('Широта', db_index=True, blank=True)
-    lon = models.FloatField('Долгота', db_index=True, blank=True)
+    lat = models.FloatField('Широта', db_index=True, null=True)
+    lon = models.FloatField('Долгота', db_index=True, null=True)
     creation_date = models.DateField(verbose_name='Дата добавления записи', default=timezone.now, db_index=True)
 
     class Meta:
